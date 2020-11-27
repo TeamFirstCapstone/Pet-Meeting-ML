3c4429404c4ff1e2c21fcace5609f6cc52761548
carnival77 <stam0325@hanmail.net>
2020-11-27 20:21:13 +0900

    add response to show_off_page

--- choosing_page.py
+++ choosing_page.py
@@ -17,16 +17,19 @@ top_n = defaultdict(list)
 app = Flask(__name__)
 
 # db 접속
+
+
 def get_connection():
     conn = pymysql.connect(host='petmeeting.cvsejgvxoucu.us-east-2.rds.amazonaws.com',
-	 user='admin', password='petmeeting123', db='petmeeting', charset='utf8')
+                           user='admin', password='petmeeting123', db='petmeeting', charset='utf8')
     return conn
 
 # {pet_id, user_id, Rating} 형식으로, 모든 평가 데이터를 가져온다.
+
+
 def get_default_ratings():
 
     conn = get_connection()
-
 
     sql = '''
 		SELECT k.PID, v.UID, v.Score FROM 
@@ -34,19 +37,19 @@ def get_default_ratings():
 		FROM petmeeting.Pet p, petmeeting.Showoff s 
 		WHERE p.UID = s.UID) AS k
 		LEFT JOIN petmeeting.Vote AS v ON k.SID = v.SID;
-    ''' 
+    '''
 
     cursor = conn.cursor()
     cursor.execute(sql)
     row = cursor.fetchall()
-	
+
     data_list = []
 
-    for obj in row :
+    for obj in row:
         data_dic = {
-            'petId' : obj[0],
-            'userId' : obj[1],
-			'rating' : obj[2],
+            'petId': obj[0],
+            'userId': obj[1],
+         			'rating': obj[2],
         }
         data_list.append(data_dic)
 
@@ -56,7 +59,7 @@ def get_default_ratings():
 
 # 유저별 rating을 기반으로, 가장 관련성이 높은 10개의 pet_id를 산출한다.
 def get_top_n(predictions, n):
-    
+
     temp = defaultdict(list)
     top_n = dict()
 
@@ -68,7 +71,7 @@ def get_top_n(predictions, n):
     for user_id, user_ratings in temp.items():
         user_ratings.sort(key=lambda x: x[1], reverse=True)
         pet_id_list = list(map(lambda x: x[0], user_ratings))
-        
+
         # pet_id_list 에서 nan 값 제거
         pet_id_list_len = len(pet_id_list)
         j = -1
@@ -78,7 +81,7 @@ def get_top_n(predictions, n):
                 del pet_id_list[j]
                 j = j-1
 
-        pet_id_list = list(map(int,pet_id_list))
+        pet_id_list = list(map(int, pet_id_list))
         top_n[user_id] = pet_id_list[:n]
 
     return top_n
@@ -94,14 +97,11 @@ def predict():
 	json_data = get_default_ratings()
 	data = pd.DataFrame(json_data)
 
-	print("data-------------------------------------------------------------")
-	print(data)
-
-	reader=Reader(rating_scale=(0,5))
-	data = Dataset.load_from_df(df=data,reader=reader)
+	reader = Reader(rating_scale=(0, 5))
+	data = Dataset.load_from_df(df=data, reader=reader)
 
 	trainset_2, testset_2 = train_test_split(data, test_size=0.3)
-	
+
 	algo = SVDpp()
 	predictions = algo.fit(trainset_2).test(testset_2)
 
@@ -111,7 +111,7 @@ def predict():
 # choosing_page에 접속했을 때, user_id를 받아와서 top_10 pet id를 return
 # /pets -> /choosing_page
 # user_id = request.args.get("uid", 0) -> user_id = requests.user_id
-@app.route('/choosing_page',methods=['GET','POST'])
+@app.route('/choosing_page', methods=['GET', 'POST'])
 def give_top_n():
 	global user_id
 	user_id = request.args.get("uid", 0)
@@ -121,7 +121,31 @@ def give_top_n():
 	    return str(top_n[int(user_id)])
 	except:
 		return str([])
-	
+
+
 if __name__ == '__main__':
     predict()
     app.run()
+
+# show off page 에 user가 새로운 vote를 할 때 db가 변하므로 새로 model을 train 시켜야 한다.
+# @app.route('/show_off_page', methods=['GET', 'POST'])
+# def new_predict():
+#     global top_n
+#     global user_id
+#     print("--predict restart--------------------------------")
+
+#     # request
+#     json_data = get_default_ratings()
+
+#     # new_data_set
+#     data = pd.DataFrame(json_data)
+
+#     reader = Reader(rating_scale=(0, 5))
+#     data = Dataset.load_from_df(df=data, reader=reader)
+
+#     trainset_2, testset_2 = train_test_split(data, test_size=0.3)
+
+#     algo = SVDpp()
+#     predictions = algo.fit(trainset_2).test(testset_2)
+
+#     top_n = get_top_n(predictions, n=10)
