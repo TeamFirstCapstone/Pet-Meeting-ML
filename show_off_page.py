c417129ce813c0178bd5be57b92b466e4275e2a6
carnival77 <stam0325@hanmail.net>
2020-11-27 22:21:08 +0900

    rearrange code

--- show_off_page.py
+++ show_off_page.py
@@ -1,24 +1,12 @@
 from sklearn.decomposition import TruncatedSVD
-from scipy.sparse.linalg import svds
-import string
 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import pandas as pd
 import numpy as np
-import math
-from surprise import Dataset
-import sys
-
-from surprise import Reader
-from surprise.model_selection import cross_validate
-from surprise import SVDpp
-from surprise.model_selection import train_test_split
-from collections import defaultdict
 
 import utils
 
 pet_ID_list=[]
-
 
 app = Flask(__name__)
 
@@ -30,10 +18,7 @@ app = Flask(__name__)
 def predict():
     global petId
     global corr
-    # get_pet_id = request.args.get("pid", 0)
     data = pd.DataFrame(utils.get_default_ratings())
-
-    # print(data)
 
     user_pet_rating = data.pivot_table('rating', index='userId',
                                        columns='petId').fillna(0)
@@ -46,44 +31,22 @@ def predict():
     corr = np.corrcoef(matrix)
 
     petId = user_pet_rating.columns
-    # pet_ID_list = list(petId)
-
-    # ##
-    # print(pet_ID_list)
-    # ##
-
-    # coffey_hands = pet_ID_list.index(get_pet_id)
-
-    # corr_coffey_hands = corr[coffey_hands]
-    # pet_id_list = list(petId[(corr_coffey_hands >= 0.9)])[:10]
-
-    # print(pet_id_list)
-
 
 @app.route('/show_off_page', methods=['GET', 'POST'])
 def give_pet_id_list():
-    global get_pet_id
     get_pet_id = request.args.get("pid", 0)
     global petId
     global corr
 
     pet_ID_list = list(petId)
 
-    ##
-    print(pet_ID_list.index(2))
-    ##
-
     coffey_hands = pet_ID_list.index(int(get_pet_id))
 
     corr_coffey_hands = corr[coffey_hands]
     pet_id_list = list(petId[(corr_coffey_hands >= 0.9)])[:10]
 
-    # print(pet_id_list)
-
     return str(pet_id_list)
-
 
 if __name__ == '__main__':
     predict()
-    # index()
     app.run()
